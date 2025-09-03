3cb205d8db3fd9779be5598097cd66ce440614cf
amudhan17 <127429047+amudhan17@users.noreply.github.com>
2025-09-03 10:23:54 +0530

    Add files via upload

---
+++ Exercise_2/Q1/q_1.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#define SIZE 50
+
+int main(){
+	printf("Enter production rule(A -> {rule1|rule2})\n");
+	printf("A-> ");
+
+	char buf[SIZE];
+	scanf("%s",buf);
+
+	char rule1[SIZE], rule2[SIZE];
+	int i=0,j=0;
+
+	while(buf[i]!='|') rule1[j++] = buf[i++];
+	rule1[j] = '\0';
+	i++;
+
+	j=0;
+	while(buf[i]!='\0') rule2[j++] = buf[i++];
+	rule2[j] = '\0';
+
+	int k=0;
+	i=j=0;
+	char prefix[SIZE];
+	while(rule1[i]==rule2[j] && rule1[i] != '\0' && rule2[j]!= '0'){
+		prefix[k++]=rule1[i++];
+		j++;
+	}
+	prefix[k] ='\0';
+
+
+	printf("---\n");
+	if(rule1[i] == rule2[j]){
+		printf("A -> %s\n",prefix);
+		return 0;
+	}
+
+	printf("A -> %sA'\n",prefix);
+	if(rule1[i] == '\0'){
+		printf("A' -> %s\n",(char*)rule2+j);
+	}
+	else if(rule2[j] == '\0'){
+		printf("A' -> %s\n",(char*)rule1+i);
+	}
+	else{
+		printf("A' -> %s|%s\n",(char*)rule1+i,(char*)rule2+j);
+	}
+	return 0;
+}
